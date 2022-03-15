ed69a2e5a30a04a2633b6a7b75b463ddedb4dc2a
stevenetal <76971042+stevenetal@users.noreply.github.com>
2022-03-15 02:52:01 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @stevenetal
+- 👀 I’m interested in ... machine learning
+- 🌱 I’m currently learning ... python
+- 💞️ I’m looking to collaborate on ... becoming an amazing programmer 
+- 📫 How to reach me ...@whatsapp:08136090065, Facebook,Morgan Steven.
+
+<!---
+stevenetal/stevenetal is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
